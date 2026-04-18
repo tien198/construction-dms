dbb741e37fb5e9b41fd243ec5fbe7cc6ff4a08b1
tien <tienvn998@gmail.com>
2026-04-18 23:35:09 +0700

    repair Domain and rename some to comply DDD

--- src/construction/document/document.module.ts
+++ src/construction/document/document.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DocumentController } from './infrastructure/adapter/inbound/document.controller';
 import { DocumentSubmissionService } from './application/service/document-submission.service';
-import { PgDocumentRepository } from './infrastructure/adapter/outbound/database/pg-document.repository';
-
-import { UnitOfWork } from './infrastructure/adapter/outbound/database/unit-of-work';
 import { DocumentQueriesService } from './application/service/document.queries.service';
-import { PgConstructionRepository } from './infrastructure/adapter/outbound/database/pg-construction.repository';
+
+import { UnitOfWork } from './infrastructure/adapter/outbound/persistence/unit-of-work';
+import { PgDocumentRepository } from './infrastructure/adapter/outbound/persistence/pg-document.repository';
+import { PgConstructionRepository } from './infrastructure/adapter/outbound/persistence/pg-construction.repository';
 
 @Module({
   controllers: [DocumentController],
